a0582915ca3bb5923ac37b421dd8c973d692eaf0
Ramadhan16-liv <safriati74@gmail.com>
2024-01-15 11:35:26 +0800

    praktek 1

---
+++ Praktek1.js
@@ -0,0 +1,22 @@
+// const tagihan = 100000;
+// const tip = 0.25
+// const nilaiTip = tagihan * tip
+// console.log(
+//     `Tagihan sejumlah ${tagihan}, tip ${nilaiTip}, total bayar ${tagihan + nilaiTip}`
+// );
+
+
+// const beliBaju  = 4;
+// const hargaBaju = 50000;
+// const tagihan = beliBaju + hargaBaju;
+
+// if (beliBaju >= 3) {
+//     const diskon1 = 0.35
+//     console.log(`total diskon yang didapatkan ${diskon1 * hargaBaju}`)
+// } else {
+//     const diskon2 = 0.05
+//     console.log(`total diskon yang didapatkan ${diskon2 * hargaBaju}`)
+// }
+
+
+
